4727bc549529cf0da5fc70bb5dee2ed7a5076add
Vincent Hellot <hellvinz@gmail.com>
2012-10-05 17:44:39 +0200

    remove useless functions + documentation

--- purger.go
+++ purger.go
@@ -17,16 +17,29 @@ var clients clientPool
 var context zmq.Context
 var logger *syslog.Writer
 
-var incomingAddress = flag.String("i", "0.0.0.0:8081", "incoming zmq purge address, eg: '0.0.0.0:8081'")
-var outgoingAddress = flag.String("o", "0.0.0.0:8080", "listening socket where purge message are sent to varnish reverse cli, eg: 0.0.0.0:8080")
-
 func main() {
+	// parse command line options
+	var incomingAddress = flag.String("i", "0.0.0.0:8081", "incoming zmq purge address, eg: '0.0.0.0:8081'")
+	var outgoingAddress = flag.String("o", "0.0.0.0:8080", "listening socket where purge message are sent to varnish reverse cli, eg: 0.0.0.0:8080")
 	flag.Parse()
 
+	// log to syslog
 	logger, _ = syslog.New(syslog.LOG_INFO, "")
+
+	// setup zmq
 	context, _ = zmq.NewContext()
 	defer context.Close()
-	go setupPurgeReceiver()
+
+	// the zmq REP socket where to send purge requests
+	go setupPurgeReceiver(outgoingAddress)
+
+	// we're ready to listen varnish cli connection
+	setupPurgeSenderAndListen(incomingAddress)
+}
+
+//setupPurgeSenderAndListen create a clientPool and start listening to the socket where varnish cli connects
+//when a client connects it is calling the handleConnection handler
+func setupPurgeSenderAndListen(incomingAddress *string) {
 	clients = make(clientPool, 0)
 	go ping()
 	defer clients.close()
@@ -38,12 +51,20 @@ func main() {
 			// handle error
 			continue
 		}
-		go handleConnection(conn)
+		logger.Info(fmt.Sprintln("New client: ", conn.RemoteAddr()))
+		// flush the whole cache of the new client
+		sendPurge(conn, ".*")
+		// put it in the client pool
+		clients = append(clients, conn)
+		// connect client to the pubsub purge
+		go connectClientToPusher(conn)
 	}
-
+	return
 }
 
-func setupPurgeReceiver() (err error) {
+//setupPurgeReceiver set up the zmq REP socket where ban messages arrives
+//when a purge pattern is received it dispatch it to a PUB socket
+func setupPurgeReceiver(outgoingAddress *string) {
 	receiver, _ := context.NewSocket(zmq.REP)
 	defer receiver.Close()
 	receiver.Bind("tcp://" + *outgoingAddress)
@@ -59,16 +80,7 @@ func setupPurgeReceiver() (err error) {
 	return
 }
 
-func handleConnection(conn net.Conn) {
-	logger.Info(fmt.Sprintln("New client: ", conn.RemoteAddr()))
-	// flush the whole cache
-	sendPurge(conn, ".*")
-	// put it in the client pool
-	clients = append(clients, conn)
-	// connect client to the pubsub purge
-	go connectClientToPusher(conn)
-}
-
+//connectClientToPusher is used to forward message received from the internal PUB socket to the client
 func connectClientToPusher(conn net.Conn) {
 	puller, _ := context.NewSocket(zmq.SUB)
 	puller.SetSockOptString(zmq.SUBSCRIBE, "")
@@ -86,6 +98,7 @@ func connectClientToPusher(conn net.Conn) {
 	}
 }
 
+//remove remove a client connection from the clientPool
 func remove(conn net.Conn) {
 	newClients := make(clientPool, 0)
 	for _, client := range clients {
@@ -97,12 +110,14 @@ func remove(conn net.Conn) {
 	return
 }
 
+//close close every connection with clients
 func (clients clientPool) close() {
 	for _, client := range clients {
 		client.Close()
 	}
 }
 
+//ping send ping message to every clients every 5 seconds
 func ping() {
 	for {
 		time.Sleep(5 * time.Second)
@@ -117,6 +132,8 @@ func ping() {
 	}
 }
 
+//sendPurge send a purge message to a client
+//it appends a ban.url to the pattern passed
 func sendPurge(conn net.Conn, pattern string) (err error) {
 	n, err := conn.Write([]byte("ban.url " + pattern + "\n"))
 	if n == 0 {
@@ -126,6 +143,7 @@ func sendPurge(conn net.Conn, pattern string) (err error) {
 	return
 }
 
+//checkError basic error handling
 func checkError(err error) {
 	if err != nil {
 		logger.Crit(fmt.Sprintln("Fatal error", err.Error()))
