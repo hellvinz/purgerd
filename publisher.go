7ce7f25e61dcf822431c11021c10ba86ca01e316
Vincent Hellot <hellvinz@gmail.com>
2013-10-29 00:28:03 +0100

    avoid data races on subscribers

--- publisher.go
+++ publisher.go
@@ -1,21 +1,23 @@
 package main
 
 import "sync/atomic"
+import "sync"
 
 type Publisher struct {
 	subscribers            []chan []byte
 	toberemovedsubscribers chan chan []byte
-	tobeaddedsubscribers chan chan []byte
+	tobeaddedsubscribers   chan chan []byte
 	Publishes              int64
+	m                      sync.Mutex
 }
 
 func NewPublisher() *Publisher {
-    publisher := Publisher{}
-    publisher.toberemovedsubscribers = make(chan chan []byte)
-    publisher.tobeaddedsubscribers = make(chan chan []byte)
-    publisher.Publishes = 0
-    go publisher.monitorsubscription()
-    return &publisher
+	publisher := Publisher{}
+	publisher.toberemovedsubscribers = make(chan chan []byte)
+	publisher.tobeaddedsubscribers = make(chan chan []byte)
+	publisher.Publishes = 0
+	go publisher.monitorsubscription()
+	return &publisher
 }
 
 func (p *Publisher) Sub(c chan []byte) {
@@ -27,28 +29,34 @@ func (p *Publisher) Unsub(c chan []byte) {
 }
 
 func (p *Publisher) Pub(message []byte) {
-	atomic.AddInt64(&p.Publishes,1)
+	atomic.AddInt64(&p.Publishes, 1)
+	p.m.Lock()
 	for _, c := range p.subscribers {
 		c <- message
 	}
+	p.m.Unlock()
 }
 
 func (p *Publisher) monitorsubscription() {
-    var c chan []byte
+	var c chan []byte
 
 	for {
-        select {
-        case c = <-p.toberemovedsubscribers:
-            var i = 0
-            var v chan []byte
-            for i, v = range p.subscribers {
-                if v == c {
-                    break
-                }
-            }
-            p.subscribers = append(p.subscribers[:i], p.subscribers[i+1:]...)
-        case c = <-p.tobeaddedsubscribers:
-            p.subscribers = append(p.subscribers, c)
-        }
+		select {
+		case c = <-p.toberemovedsubscribers:
+			var i = 0
+			var v chan []byte
+			p.m.Lock()
+			for i, v = range p.subscribers {
+				if v == c {
+					break
+				}
+			}
+			p.subscribers = append(p.subscribers[:i], p.subscribers[i+1:]...)
+			p.m.Unlock()
+		case c = <-p.tobeaddedsubscribers:
+			p.m.Lock()
+			p.subscribers = append(p.subscribers, c)
+			p.m.Unlock()
+		}
 	}
 }
